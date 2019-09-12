fae780d68562f225ff1aac4647013327d48ca3f7
HIMANI <himani.rathi@stanzaliving.com>
2019-09-12 12:00:49 +0530

    using module.export to resolve error

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import Button from './components/Button';
-import SearchBar from './components/SearchBar';
-import AssistiveTouch from './components/AssistiveTouch';
+// import Button from './components/Button';
+// import SearchBar from './components/SearchBar';
+// import AssistiveTouch from './components/AssistiveTouch';
 
-export default{
-    Button,
-    SearchBar,
-    AssistiveTouch
-}
+module.exports = {
+  get Button() {
+    return require('./components/Button').default;
+  },
+  get SearchBar() {
+    return require('./components/SearchBar').default;
+  },
+  get AssistiveTouch() {
+    return require('./components/AssistiveTouch').default;
+  }
+};
+// export default {
+//     Button,
+//     SearchBar,
+//     AssistiveTouch
+// }
