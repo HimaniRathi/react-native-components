0cf93897f2eee8b0930532965ebd702d41a072ee
HIMANI <himani.rathi@stanzaliving.com>
2019-09-12 12:56:55 +0530

    adding dependency @babel/runtime

--- index.js
+++ index.js
@@ -1,74 +1,20 @@
-// import Button from './components/Button';
-// import SearchBar from './components/SearchBar';
-// import AssistiveTouch from './components/AssistiveTouch';
+import Button from './components/Button';
+import SearchBar from './components/SearchBar';
+import AssistiveTouch from './components/AssistiveTouch';
 
-// // module.exports = {
-// //   get Button() {
-// //     return require('./components/Button').default;
-// //   },
-// //   get SearchBar() {
-// //     return require('./components/SearchBar').default;
-// //   },
-// //   get AssistiveTouch() {
-// //     return require('./components/AssistiveTouch').default;
-// //   }
-// // };
-// export default {
-//     Button,
-//     SearchBar,
-//     AssistiveTouch
-// }
-import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, Dimensions} from 'react-native';
-import PropTypes from 'prop-types';
-
-export default class Button extends React.Component{ 
-    static propTypes = {
-        buttonText: PropTypes.string,
-        buttonType: PropTypes.string,
-        handleButtonPress: PropTypes.func,
-        style: PropTypes.object,
-        buttonStretch: PropTypes.bool
-    }
-    static defaultProps = {
-        buttonText: "Submit",
-        buttonType: "btn-default",
-        handleButtonPress: null,
-        style: null,
-        buttonStretch: false
-    }
-    render(){
-        let {buttonType} = this.props;
-        return(
-            <View>
-                <TouchableOpacity 
-                    onPress = {() => {this.props.handleButtonPress && this.props.handleButtonPress()}}
-                >
-                    <Text 
-                        style = {[
-                            styles.buttonStyle,
-                            this.props.buttonStretch ? {width: Dimensions.get("window").width} : {},
-                            buttonType == 'btn-default' ? {borderWidth: 1} :
-                            buttonType == 'btn-primary' ? {backgroundColor: "#337ab7", color: 'white'} : 
-                            buttonType == 'btn-success' ? {backgroundColor: "#5cb85c", color: 'white'} : 
-                            buttonType == 'btn-info' ? {backgroundColor: "#5bc0de", color: 'white'} : 
-                            buttonType == 'btn-warning' ? {backgroundColor: "#f0ad4e", color: 'white'} : 
-                            buttonType == 'btn-danger' ? {backgroundColor: "#d9534f", color: 'white'} : 
-                            buttonType == 'btn-link' ? {color: '#337ab7'} : null,
-                            this.props.style
-                        ]}
-                    >
-                        {this.props.buttonText}
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+// module.exports = {
+//   get Button() {
+//     return require('./components/Button').default;
+//   },
+//   get SearchBar() {
+//     return require('./components/SearchBar').default;
+//   },
+//   get AssistiveTouch() {
+//     return require('./components/AssistiveTouch').default;
+//   }
+// };
+export default {
+    Button,
+    SearchBar,
+    AssistiveTouch
 }
-const styles = StyleSheet.create({
-    buttonStyle: {
-        padding: 10,
-        textAlign: 'center',
-        borderRadius: 3
-    }
-})
